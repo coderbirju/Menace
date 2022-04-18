0f062b8d79f190ced5f1f6858c1766f0e058bb99
Arjun Raja Yogidas <arjunbirju@Arjuns-MacBook-Air.local>
2022-04-17 22:51:39 -0400

    checkpoint 1: api calls happening, test move api now

--- tictactoefe/src/components/Game.js
+++ tictactoefe/src/components/Game.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Board from './Board';
-import { calculateWinner } from '../helpers/helper';
+import { calculateWinner, getCurrentBoardStateString, getPositionString } from '../helpers/helper';
 import axios from 'axios';
 
 const styles = {
@@ -11,30 +11,9 @@ const styles = {
 function Game() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(true);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(board); 
 
-  
-
-
-  const handleClick =  async (i) => {
-    const tempBoard = [...board];
-    let cbs = "";
-    
-    let test = [[0,0,0], [1,0,0], [-1,-1,1]];
-    for(var iter in test){
-      let c = JSON.stringify(test[iter]);
-      cbs = cbs + c + ",";
-      console.log(c, typeof c);
-    }
-    cbs.slice(0, -1);
-    
-    var data = JSON.stringify({
-      position: "[(0,0),(0,1),(0,2),(1,1),(1,2)]",
-      current_board_state: cbs,
-      symbol: -1
-    });
-
-    console.log('data: ', data);
+  const callApi = async (data) => {
     var config = {
       method: 'post',
       url: 'http://127.0.0.1:5000/api/move',
@@ -43,21 +22,48 @@ function Game() {
       },
       data : data
     };
-    
-    
     const response = await axios(config);
-  
     console.log('response: ', response);
-    
+    return response;
+  }
+
+
+  const handleClick =  async (i) => {
+    const tempBoard = [...board];
     if(winner || tempBoard[i]) return;
     tempBoard[i] = isXNext ? 'X' : 'O';
     setBoard(tempBoard);
     setIsXNext(!isXNext); 
   }
 
-  const jumpTo = () => {
+  useEffect(() => {
+    async function callApiHelper() {
+      const boardState = getCurrentBoardStateString(board);
+      const freePositions = getPositionString(board);
+      console.log('boardState: ', boardState);
+      console.log('freePositions: ', freePositions);
+      console.log('board: ', board);
+      let cbs = "";
+      let test =  boardState;
+      for(var iter in test){
+        let c = JSON.stringify(test[iter]);
+        cbs = cbs + c + ",";
+      }
+      cbs.slice(0, -1);
+      
+      var data = JSON.stringify({
+        position: freePositions,
+        current_board_state: cbs,
+        symbol: -1
+      });
+  
+      return await callApi(data);
+    }
 
-  }
+    if(board) {
+      callApiHelper()
+    }
+}, [board]);
 
   const renderMoves = () => (
     <button style={{ marginLeft: '100px', paddingLeft: '10px' }} onClick={() => setBoard(Array(9).fill(null))}>
--- tictactoefe/src/helpers/helper.js
+++ tictactoefe/src/helpers/helper.js
@@ -18,10 +18,62 @@ export function calculateWinner(squares) {
     return null;
 }
 
-// const squares = [
-//     'O', null, null,
-//     'X', null, 'X',
-//     null, null, 'O'
-// ]
+export function getPositionString(board) {
+    let freePositions = "[";
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8]
+    ];
+    for(let i = 0; i<lines.length; i++){
+        const [a,b,c] = lines[i];
+        if(board[a] === null) {
+            let pos = `(${i},  ${(a % 3)}),`;
+            freePositions = freePositions + pos;
+        } 
+        if(board[b] === null) {
+            let pos = `(${i},  ${(b % 3)}),`;
+            freePositions = freePositions + pos;
+        } 
+        if(board[c] === null) {
+            let pos = `(${i},  ${(c % 3)}),`;
+            freePositions = freePositions + pos;
+        }
+    }
+    freePositions.slice(0, -1);
+    freePositions = freePositions + "]";
+    return freePositions;
+}
 
-// console.log(calculateWinner(squares));
+export function getCurrentBoardStateString(board) {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8]
+    ];
+    let boardState = [];
+    for(let i = 0; i<lines.length; i++){
+        const [a,b,c] = lines[i];
+        let row = [0,0,0];
+        if(board[a] === "X") {
+            row[0] = 1;
+        } else if(board[a] === "O"){
+            row[0] = -1;
+        }
+
+        if(board[b] === "X") {
+            row[1] = 1;
+        } else if(board[b] === "O"){
+            row[1] = -1;
+        }
+
+        if(board[c] === "X") {
+            row[2] = 1;
+        } else if(board[c] === "O"){
+            row[2] = -1;
+        }
+        boardState.push(row);
+    }
+    console.log('boardState: ', boardState);
+    return boardState;
+}
